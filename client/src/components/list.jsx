55e733c7ebefa339a775d9ba28fba3e01f09864b
Adam Reback <adamreback@Adams-MacBook-Pro.local>
2019-03-29 20:25:56 -0700

    Backend | backend completed and seeded pending additional dummy data Client  | Photo's rendered and most components completed.  Css forthcoming

--- client/src/components/list.jsx
+++ client/src/components/list.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ListEntry from './listEntry.jsx';
+import React from "react";
+import ListEntry from "./listEntry.jsx";
 
-const List = (props) => {
+const List = props => {
   return (
-    <div>hello</div>
-  )
-}
+    <div>
+      {props.imageList.map((item, index) => {
+        return <ListEntry pic={item} key={index} />;
+      })}
+    </div>
+  );
+};
 
 export default List;
--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const parser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
 const router = require('./router.js');
-const port = 3000;
+const port = 3001;
 const server = express();
 
 server.use(morgan('dev'));
 server.use(parser.json());
-server.use(parser.urlencoded({extended:true}));
+server.use(parser.urlencoded({
+  extended: true
+}));
 
 server.use('/api', router)
 server.use(express.static(path.join(__dirname, '/../client/dist')))
