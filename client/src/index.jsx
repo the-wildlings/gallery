b1e2ba32e1b8d87c686c1433d643cbf399834c4c
Adam Reback <adamreback@Adams-MacBook-Pro.local>
2019-04-11 19:00:32 -0700

    Fixing default gitignore problems

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app.jsx";
+import Gallery from "./components/app.jsx";
+import "./style.css";
+import "./bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById("gallery"));
+ReactDOM.render(<Gallery />, document.getElementById("gallery"));
+// window.Gallery = Gallery;
