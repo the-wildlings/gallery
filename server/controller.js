61e0b1eeb871972d1df730560f279eb9dcafe88e
ohannieho <ohannieho123@yahoo.com>
2019-04-24 13:52:57 -0700

    added routes and controller

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,5 @@
 // const db = require('../database/index.js'); //mongodb
-const db = require('../database/Model.js');
+const db = require('../database/index.js');
 
 const controller = {
   get: (req, res) => {
@@ -16,6 +16,27 @@ const controller = {
     db.find()
       .then(data => res.status(200).send(data))
       .catch(err => res.status(404).send(err));
+  },
+
+  post: (req, res) => {
+    let { id, title, location, urls } = req.body;
+    db.insertMany({ id, urls, title, location })
+      .then(() => res.status(201).send('posted'))
+      .catch(err => console.error(err));
+  },
+
+  delete: (req, res) => {
+    let { id } = req.params;
+    db.deleteOne({ id })
+      .then(() => res.status(203).send('deleted'))
+      .catch(err => console.error(err));
+  },
+
+  update: (req, res) => {
+    let { id } = req.params;
+    db.updateOne({ id }, req.body)
+      .then(() => res.status(202).send('updated'))
+      .catch(err => console.error(err));
   }
 };
 
